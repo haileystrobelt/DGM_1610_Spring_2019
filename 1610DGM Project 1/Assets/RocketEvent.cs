95def2225d2e84fb3848233ab409b4fb24682db1
Hailey Strobelt <haileystrobelt@gmail.com>
2019-02-04 10:20:37 -0700

    Fixed Unity Project Location

---
+++ 1610DGM Project 1/Assets/RocketEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RocketEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    private void OnCollisionEnter2D()
+    {
+        Event.Invoke();
+    }
+}
