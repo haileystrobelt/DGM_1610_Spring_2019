7203ac6c72382dabc364247a32abee95e2766691
Hailey Strobelt <haileystrobelt@gmail.com>
2019-02-05 20:11:49 -0700

    Project

---
+++ 1610DGM Project 1/Assets/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+    public string Name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
